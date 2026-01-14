54a1ada8fd9abb7f6ba3512b340007eb946eeabb
StrMnL <sitarui78@gmail.com>
2026-01-14 13:17:07 +0800

    chore: 修正了配置文件中的站点地址，并修改了一些文字

--- src/config.ts
+++ src/config.ts
@@ -23,8 +23,8 @@ const SITE_LANG = "zh_CN"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UTC+8
 export const siteConfig: SiteConfig = {
 	title: "偏微分茶会",
-	subtitle: "Tea & Inequality",
-	siteURL: "https://monlark.top/", // 请替换为你的站点URL，以斜杠结尾
+	subtitle: "Kappa Tea!",
+	siteURL: "https://www.monlark.top/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2025-11-10", // 站点开始运行日期，用于站点统计组件计算运行天数
 
 	timeZone: SITE_TIMEZONE,
